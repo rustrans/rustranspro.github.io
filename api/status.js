59983011fbb564f0c106a5a39e4e2e0c502a4be8
rustrans <47042809+rustrans@users.noreply.github.com>
2026-05-03 13:52:24 +0300

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,14 @@
+import { Redis } from '@upstash/redis';
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  const status = await redis.get('status');
+
+  res.json({
+    text: status || "Статус не задан"
+  });
+}
